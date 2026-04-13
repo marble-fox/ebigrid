1cbe4ef648f91e8c356ee211ac9a8070381cc030
marble-fox <snowfox@disroot.org>
2026-04-13 18:23:18 +0300

    Initial commit

---
+++ test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"ebigrid/grid"
+	"fmt"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct {
+	Grid           *grid.RectGrid
+	shiftX, shiftY int
+}
+
+func (g *Game) Update() error {
+	speed := 3
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		g.shiftX -= speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		g.shiftX += speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		g.shiftY -= speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		g.shiftY += speed
+	}
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	g.Grid.DebugDraw(screen, g.shiftX, g.shiftY, false)
+
+	fps := ebiten.ActualFPS()
+	tps := ebiten.ActualTPS()
+
+	x, y := ebiten.CursorPosition()
+	cellX, cellY, exists := g.Grid.CellCoordinatesFromPos(x+g.shiftX, y+g.shiftY)
+
+	ebitenutil.DebugPrint(screen, fmt.Sprint(
+		"FPS: ", fps,
+		"\nTPS: ", tps,
+		"\nCursor: ", x, y,
+		"\nCell: ", cellX, cellY, exists,
+	))
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	game := Game{
+		Grid: grid.NewRectGrid(
+			30,
+			30,
+			0,
+			0,
+			30,
+			30,
+		),
+		shiftX: 0,
+		shiftY: 0,
+	}
+
+	ebiten.SetWindowSize(1296, 810)
+	//ebiten.SetFullscreen(true)
+
+	err := ebiten.RunGame(&game)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
